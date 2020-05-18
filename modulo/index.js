7792dc7b01583c7a24c77f6ab5db31df604fddb8
jhonPariona <wilder.code.27@gmail.com>
2020-05-17 21:09:14 -0500

    🔥 ADD modulo

---
+++ modulo/index.js
@@ -0,0 +1,9 @@
+import { hello } from "./module_one.js"
+
+import {module2, module3} from "./modules"
+
+console.log(module2(" From module 2"));
+console.log(hello("from module"));
+console.log(module3(" from module 3"));
+
+
---
+++ modulo/modules/index.js
@@ -0,0 +1,2 @@
+export {module2} from "./module2/module2"
+export { default as module3 } from "./module3/module3"
